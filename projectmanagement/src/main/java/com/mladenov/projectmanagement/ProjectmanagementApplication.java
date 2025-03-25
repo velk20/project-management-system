edc4e560598b5658c61caba25c3ce7b29ee5e70c
Angel Mladenov <a.mladenov@seeburger.com>
2025-03-25 13:53:20 +0200

    Swagger beareer auth

--- projectmanagement/src/main/java/com/mladenov/projectmanagement/ProjectmanagementApplication.java
+++ projectmanagement/src/main/java/com/mladenov/projectmanagement/ProjectmanagementApplication.java
@@ -1,8 +1,29 @@
 package com.mladenov.projectmanagement;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Stream Line API",
+				version = "1.0",
+				description = "API documentation for Project Management System",
+				contact = @Contact(name = "Angel Mladenov", email = "angelmladenov3@gmail.com")
+		),
+		security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+		name = "bearerAuth",
+		type = SecuritySchemeType.HTTP,
+		scheme = "bearer",
+		bearerFormat = "JWT"
+)
 @SpringBootApplication
 public class ProjectmanagementApplication {
 
