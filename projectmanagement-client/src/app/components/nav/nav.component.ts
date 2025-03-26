a470b10bca45ec4ae4cec9c274f4f8539225dcfa
Angel Mladenov <a.mladenov@seeburger.com>
2025-03-26 15:11:54 +0200

    Home, Nav and Footer

---
+++ projectmanagement-client/src/app/components/nav/nav.component.ts
@@ -0,0 +1,40 @@
+import {Component, OnInit} from '@angular/core';
+import {Router, RouterLink, RouterLinkActive} from "@angular/router";
+import {AuthService} from "../../services/auth.service";
+import {NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [
+    NgIf,
+    RouterLinkActive,
+    RouterLink
+  ],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css'
+})
+export class NavComponent implements OnInit {
+  isLoggedIn: boolean = false;
+
+  constructor(private authService: AuthService,
+              private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.authService.getJwtTokenAsObservable().subscribe((jwtToken) => {
+      this.isLoggedIn = !!jwtToken;
+    });
+  }
+
+  getLoggedUserUsername(){
+    return this.authService.getUserFromJwt().username;
+  }
+
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/']);
+  }
+
+
+}
