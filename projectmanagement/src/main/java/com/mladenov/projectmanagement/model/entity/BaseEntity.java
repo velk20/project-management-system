2b66a4622a602c80ae9266df96f6b20938eb817a
Angel Mladenov <a.mladenov@seeburger.com>
2025-03-26 14:15:20 +0200

    Refactor services

--- projectmanagement/src/main/java/com/mladenov/projectmanagement/model/entity/BaseEntity.java
+++ projectmanagement/src/main/java/com/mladenov/projectmanagement/model/entity/BaseEntity.java
@@ -4,25 +4,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    public BaseEntity() {
-    }
-
-    public BaseEntity(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
